37ba54faea9c52b8a99279a58418aa4f478776bf
Rafat khtab <rafat0khtab@gmail.com>
2025-11-09 18:45:32 +0300

    change useSearchParams to 14 v

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,22 @@
 import ProductList from "./components/ProductList";
 import Image from "next/image";
-
-export default function Home({
+export default async function Home({
   searchParams,
 }: {
-  searchParams: { category?: string };
+  searchParams: Promise<{ category?: string }>;
 }) {
-  const category = searchParams.category || "";
-
+  const params = await searchParams;
+  const category = params?.category ?? "all";
   return (
     <div>
       <div className="relative aspect-[3/1] mb-4">
         <Image src="/featured.png" alt="featured product" fill />
       </div>
       <ProductList category={category} params="Homepage" />
+       
     </div>
   );
 }
+
+
+
